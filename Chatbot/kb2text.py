93ec848ba5705e7e4a056646dc7f4d9370e2ba83
Matthew Wiebe <mattweb117@gmail.com>
2016-10-26 11:21:08 -0700

    minor updates

--- Chatbot/kb2text.py
+++ Chatbot/kb2text.py
@@ -11,11 +11,12 @@ def run():
     engine.reset()
     try:
         engine.activate('relation')
-        result = engine.prove_1_goal('relation.knows_pattern_matching()')
+        result = engine.prove_1_goal('relation.build_pizza()')
         print result
     except StandardError:
         krb_traceback.print_exc()
         sys.exit(1)
 
 
+#
 run()
